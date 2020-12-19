47a50e8bb597fb304962b3e33c699e778c7c9e54
farlic <thelewisking@gmail.com>
2020-12-19 22:14:25 +0000

    data capture improvement

--- reader.py
+++ reader.py
@@ -42,7 +42,7 @@ class Person:
 print('----------------------\ncompiling regular expressions')
 gender_pattern = re.compile("( (fe)?male)",re.I)
 relative_pattern = re.compile("((bro|mo|fa)?ther|sister|informant|causing the body to be cremated|uncle)",re.I)
-district_pattern = regex.compile('(?i)(registration district|Administrative area){e<=1}')
+district_pattern = regex.compile('(?i)(registration district|Administrative area){s<=2,d<=2,e<=3}')
 
 for index,page in enumerate(pages):
     start_time = time.time()
@@ -65,7 +65,7 @@ for index,page in enumerate(pages):
     for key,line in enumerate(text): #temp output to see lines
         #print(line)
 
-        if regex.search('(?i)(registration){e<=2}',line) and len(p.district) == 0: #(?i) is the old regex ignore case
+        if regex.search('(?i)(registration district){e<=2}',line) and len(p.district) == 0: #(?i) is the old regex ignore case
             p.district = text[key].strip()
 
         elif regex.search('(?i)(date and place of death){e<=3}',line) and len(p.dod) == 0:# {e<3} is fuzzy match
@@ -74,24 +74,25 @@ for index,page in enumerate(pages):
         elif regex.search('(?i)(name and surname){e<=2}',line) and len(p.name) == 0:
             p.name = text[key+1].strip()
 
-        elif regex.search('(?i)(married){e<=2}',line) and len(p.maiden) == 0: #this needs fixing
-            p.maiden = text[key+1].strip()
+        elif regex.search('(?i)(of woman who){e<=2}',line) and len(p.maiden) == 0: #this needs fixing
+            p.maiden = text[key].strip()
 
         elif regex.search('(?i)(date and place of birth){e<=3}',line) and len(p.dob) == 0:
             p.dob = text[key+1].strip()
+            p.pob = text[key+2].strip()
 
         elif regex.search('(?i)(date and place of birth){e<=3}',line) and len(p.pob) == 0:
             p.pob = text[key+2].strip()
 
-        elif regex.search('(?i)(usual address){e<=3}',line) and len(p.usual_address) == 0:
-            p.usual_address = text[key+2].strip()
+        elif regex.search('(?i)(name and surname of informant){e<=3}',line) and len(p.usual_address) == 0:
+            p.usual_address = text[key-1].strip()
 
         elif regex.search('(?i)(informant){e<=3}',line) and len(p.informant_name) == 0:
             p.informant_name = text[key+1].strip()
 
-        elif regex.search('(?i)(usual address){e<=3}',line) and len(p.informant_address) == 0:
-            p.informant_address = text[+key+1].strip()
-
+        elif regex.search('(?i)(certify that){e<=3}',line) and len(p.informant_address) == 0:
+            p.informant_address = text[key-1].strip()
+            p.informant_name = text[key+-3].strip()
         elif regex.search('(?i)(date of registration){e<=3}',line) and len(p.reg_date) == 0:
             p.reg_date = text[key+1].strip()
 
@@ -107,6 +108,8 @@ for index,page in enumerate(pages):
         remove_districts.append(match.span())
     p.district = p.district[remove_districts[0][1]:remove_districts[1][0]].strip()
 
+    if p.gender.lower() == 'male':
+        p.maiden = ''
     #District
     #DoD
     #Name
@@ -120,7 +123,9 @@ for index,page in enumerate(pages):
     #Informant Address
     #Date of Registration
 
-    for x,y in vars(p).items():
+    for items in vars(p).items():
+        x = items[0]
+        y = items[1]
         print(f'{x} : {y}')
 
     end_time = time.time()
